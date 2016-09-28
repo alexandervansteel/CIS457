5e497b31ce5981058f7f2c8d624f4efbbcc43a8f
alexandervansteel <alexandervansteel@gmail.com>
2016-09-28 11:47:57 -0400

    updates

--- Lab4/udpclient.c
+++ Lab4/udpclient.c
@@ -2,6 +2,8 @@
 #include <netinet/in.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 int main(int argc, char **argv){
   int sockfd = socket(AF_INET,SOCK_DGRAM,0);
@@ -10,8 +12,27 @@ int main(int argc, char **argv){
   }
   struct sockaddr_in serveraddr;
   serveraddr.sin_family = AF_INET;
-  serveraddr.sin_port = htons(9876);
-  serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  char port[10];
+  printf("Enter port number: ");
+  if(fgets(port,10,stdin) == NULL){
+    perror("invalid input");
+  } else {
+    serveraddr.sin_port = htons((int) strtol(port,(char **)NULL,10));
+  }
+//  serveraddr.sin_port = htons(9876);
+  char ip[10];
+  char *s = ip;
+  printf("Enter IP address: ");
+  if(fgets(ip,10,stdin) == NULL){
+    perror("invalid input");
+  } else {
+    int x = inet_pton(AF_INET, s, &(serveraddr.sin_addr.s_addr));
+    if(x<1){
+      printf("IP address error. Shutting down.\n");
+      return 1;
+    }
+  }
+//  serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
   printf("Enter a line: ");
   char line[1000];
--- Lab4/udpserver.c
+++ Lab4/udpserver.c
@@ -2,18 +2,39 @@
 #include <netinet/in.h>
 #include <stdio.h>
 #include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 int main(int argc, char **argv){
-  int sockfd = socket(AF_INET,SOCK_DGRAM,0);
+  int sockfd;
   if(sockfd<0){
     printf("Error making socket.\n");
   }
   struct sockaddr_in serveraddr;
+
   serveraddr.sin_family = AF_INET;
-  serveraddr.sin_port = htons(9876);
+
+  char port[10];
+  printf("Enter port number: ");
+  if(fgets(port,10,stdin) == NULL){
+    perror("invalid input");
+  } else {
+    serveraddr.sin_port = htons((int) strtol(port,(char **)NULL,10));
+  }
+//  serveraddr.sin_port = htons(9876);
   serveraddr.sin_addr.s_addr = INADDR_ANY;
 
-  bind(sockfd,(struct sockaddr*)&serveraddr,sizeof(serveraddr));
+  if((sockfd = socket(AF_INET,SOCK_DGRAM,0)) < 0){
+    perror("socket error");
+    exit(1);
+  }
+
+  if(bind(sockfd,(struct sockaddr*)&serveraddr,sizeof(serveraddr)) < 0){
+    perror("bind error");
+    exit(1);
+  } else {
+    printf("Waiting to hear from client...\n");
+  }
 
   while(1){
     int len = sizeof(struct sockaddr_in);
